0570e36f426d0d8744c80c54fe2629f7c82156d6
lazyyybonez <lbones1@gulls.salisbury.edu>
2018-04-30 16:26:23 -0400

    set up class

--- source/knapsacks.cpp
+++ source/knapsacks.cpp
@@ -3,3 +3,14 @@
 //This file will hold the functions of the knapsack class
 #include <iostream>
 #include "knapsacks.h"
+#include <vector>
+
+std::vector<int> Knapsacks::DynamicKnapsack(int itemWeight[], int itemValue[], int amount, int limit){
+
+} //dynamic implentation
+bool Knapsacks::isCorrect(std::vector<Items>& stuff, int index, int currWeight){
+
+} //checks solution
+std::vector<Items> Knapsacks::returnSolution(std::vector<Items>& stuff, std::vector<int>& solution){
+
+} //returns solution
--- source/knapsacks.h
+++ source/knapsacks.h
@@ -5,7 +5,22 @@
 #ifndef KNAPSACKS_H
 #define KNAPSACKS_H
 #include <iostream>
+#include <vector>
 
-//knapsack class decleration goes here
+struct Items{
+  int index;
+  int weight;
+  std::vector<int> appearances;
+};
+
+//knapsack class declaration goes here
+class Knapsacks{
+  public:
+    Knapsacks();
+    ~Knapsacks();
+    std::vector<int> DynamicKnapsack(int itemWeight[], int itemValue[], int amount, int limit); //dynamic implentation
+    bool isCorrect(std::vector<Items>& stuff, int index, int currWeight); //checks solution
+    std::vector<Items> returnSolution(std::vector<Items>& stuff, std::vector<int>& solution); //returns solution
+};
 
 #endif
